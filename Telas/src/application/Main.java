957191597e7069413fe4fc708b48b9afe37b3f0c
jeks <palomasousa15@gmail.com>
2016-06-25 20:54:04 -0300

    Admin tela

---
+++ Telas/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(Main.class, (java.lang.String[])null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("AdminView.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            //primaryStage.setTitle("FXML is Simple");
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ Telas/src/application/ProgressBarController.java
@@ -0,0 +1,37 @@
+package application;
+
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+
+public class ProgressBarController {
+    private Task copyWorker;
+
+    @FXML
+    private ProgressBar waterBar;
+
+    @FXML
+    private void onButtonClick() {
+         copyWorker = createWorker();
+         waterBar.progressProperty().unbind();
+         waterBar.progressProperty().bind(copyWorker.progressProperty());
+         new Thread(copyWorker).start();
+    }
+
+    
+    public Task createWorker() {
+        return new Task() {
+            @Override
+            protected Object call() throws Exception {
+                for (int i = 0; i <= 10; i++) {
+                    Thread.sleep(1000);
+                    updateMessage("1000 milliseconds");
+                    updateProgress(i, 10);
+                    System.out.println(waterBar.getProgress());
+                }
+                return true;
+            }
+        };
+    }
+    
+}
