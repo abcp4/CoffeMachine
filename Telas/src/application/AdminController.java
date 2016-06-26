431aef7fa91a3933d18f67e0abba96e6b521b4ff
jeks <palomasousa15@gmail.com>
2016-06-26 20:45:10 -0300

    Add files via upload

--- Telas/src/application/AdminController.java
+++ Telas/src/application/AdminController.java
@@ -12,6 +12,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -19,15 +20,17 @@ public class AdminController implements Initializable {
 
     @FXML //  fx:id="waterDetails"
     private Button waterDetails; // Value injected by FXMLLoader
-
+    @FXML
+    private MenuItem addIngredient, editIngredient;
 
     @Override // This method is called by the FXMLLoader when initialization is complete
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-        assert waterDetails != null : "fx:id=\"myButton\" was not injected: check your FXML file 'simple.fxml'.";
-
         // initialize your logic here: all @FXML variables will have been injected
+    	addIngredient.setOnAction(e -> openRecipeView("Cadastrar"));
+    	waterDetails.setOnAction(e -> openRecipeView("Editar"));
+    	editIngredient.setOnAction(e -> openRecipeView("Editar"));
         
-        waterDetails.setOnAction(new EventHandler<ActionEvent>() {
+        /*waterDetails.setOnAction(new EventHandler<ActionEvent>() {
         	Stage stage; 
         	Parent root;
             @Override
@@ -46,7 +49,27 @@ public class AdminController implements Initializable {
             	stage.showAndWait();
             }
         });
-
+        */
+        
 
     }
+    
+    public void openRecipeView(String title){
+    	Stage stage; 
+    	Parent root = null;
+        
+        stage = new Stage();
+        try {
+        		root = FXMLLoader.load(getClass().getResource("RecipeView.fxml"));
+			} catch (IOException e) {
+					// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        stage.setScene(new Scene(root));
+        stage.setTitle("Ingrediente::"+title);
+        stage.setResizable(false);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+   }
+    
 }
--- Telas/src/application/RecipeController.java
+++ Telas/src/application/RecipeController.java
@@ -10,10 +10,10 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.MouseEvent;
+
 
 public class RecipeController implements Initializable {
  @FXML
