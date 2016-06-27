d473b1568daa90b77a726543980164fec76ac156
jeks <palomasousa15@gmail.com>
2016-06-27 09:14:19 -0300

    Update RecipeController.java

--- Telas/src/application/RecipeController.java
+++ Telas/src/application/RecipeController.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
 
 public class RecipeController implements Initializable {
@@ -25,6 +27,12 @@ public class RecipeController implements Initializable {
  @FXML
  private Button recipeConfirm;
  
+ @FXML
+ private TextField numericField;
+ 
+ @FXML
+ private Label percentRecipe;
+ 
 @Override
 public void initialize(URL location, ResourceBundle resources) {
 	// TODO Auto-generated method stub
@@ -32,8 +40,13 @@ public void initialize(URL location, ResourceBundle resources) {
 	recipeSlider.valueProperty().addListener(new ChangeListener<Number>() {
             public void changed(ObservableValue<? extends Number> ov,
                 Number old_val, Number new_val) {
-            	 System.out.println(new_val.doubleValue());
+            	 BigDecimal bd = BigDecimal.valueOf((double) new_val);
+        		 bd = bd.setScale(2, BigDecimal.ROUND_DOWN);
+        		 Double value = bd.doubleValue();
+            	 System.out.println(bd.doubleValue());
             	 recipeBar.setProgress((double) new_val/100);
+            	 numericField.setText(value.toString());
+            	 percentRecipe.setText(value.toString() + "%");
             }
         });
 	
@@ -46,5 +59,14 @@ public void initialize(URL location, ResourceBundle resources) {
 		}
 		
 	});
+	
+	numericField.textProperty().addListener(new ChangeListener<String>() {
+        @Override
+        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+            if (!newValue.matches("\\d*")) {
+            	numericField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        }
+    });
 }
 }
